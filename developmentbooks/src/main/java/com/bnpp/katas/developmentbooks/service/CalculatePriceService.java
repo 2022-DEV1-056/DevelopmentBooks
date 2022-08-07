867ca2cf9b4f60268ec271557b776e90c82ed7e2
2022-DEV1-056 <2022dev1056@gmail.com>
2022-08-07 21:58:40 +0530

    refactor: move number values to constants

--- developmentbooks/src/main/java/com/bnpp/katas/developmentbooks/service/CalculatePriceService.java
+++ developmentbooks/src/main/java/com/bnpp/katas/developmentbooks/service/CalculatePriceService.java
@@ -13,16 +13,21 @@ import com.bnpp.katas.developmentbooks.store.DevelopmentBooksEnum;
 @Service
 public class CalculatePriceService {
 
+	private static final int HUNDRED = 100;
+	private static final int ZERO_PERCENT = 0;
+	private static final int FIVE_PERCENT = 5;
+	private static final int TWO_DISTINCT_BOOKS = 2;
+
 	public Double calculatePrice(List<BookDto> listOfBooks) {
 		Map<Integer, Double> bookIdPriceMap = Arrays.stream(DevelopmentBooksEnum.values())
 				.collect(Collectors.toMap(DevelopmentBooksEnum::getId, DevelopmentBooksEnum::getPrice));
 
 		long distinctBooks = listOfBooks.stream().map(BookDto::getId).distinct().count();
-		int discountPercentage = (distinctBooks == 2) ? 5 : 0;
+		int discountPercentage = (distinctBooks == TWO_DISTINCT_BOOKS) ? FIVE_PERCENT : ZERO_PERCENT;
 
 		double actualPrice = listOfBooks.stream()
 				.mapToDouble(book -> bookIdPriceMap.get(book.getId()) * book.getQuantity()).sum();
-		double discountedPrice = (actualPrice * discountPercentage) / 100;
+		double discountedPrice = (actualPrice * discountPercentage) / HUNDRED;
 
 		return (actualPrice - discountedPrice);
 	}
