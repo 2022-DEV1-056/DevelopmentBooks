055bd3362fccf5b25f2814736c4ee333998432ce
2022-DEV1-056 <2022dev1056@gmail.com>
2022-08-08 20:59:59 +0530

    feat: should call get books API

--- developmentbooks/src/main/frontend/src/screen/Dashboard.js
+++ developmentbooks/src/main/frontend/src/screen/Dashboard.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "../styles/dashboard.css";
+import axios from "axios";
 
 const Dashboard = () => {
+  useEffect(() => {
+    axios.get("http://localhost:8080/api/developmentbooks/getBooks");
+  }, []);
   return (
     <div className="container">
       <div className="header">
--- developmentbooks/src/main/frontend/src/test/screen/Dashboard.test.js
+++ developmentbooks/src/main/frontend/src/test/screen/Dashboard.test.js
@@ -1,5 +1,11 @@
 import { render } from "@testing-library/react";
 import Dashboard from "../../screen/Dashboard";
+import axios from "axios";
+
+afterEach(() => {
+  axios.get.mockClear();
+});
+jest.mock("axios");
 
 describe("load development books dashboard", () => {
   test("should display development dashboard title", () => {
@@ -7,6 +13,16 @@ describe("load development books dashboard", () => {
 
     expect(container.querySelector(".title")).toContainHTML(
       "Development Books"
+    );
+  });
+
+  test("should call get books API", () => {
+    axios.get.mockImplementation(() => new Promise(() => {}));
+
+    render(<Dashboard />);
+
+    expect(axios.get).toHaveBeenCalledWith(
+      expect.stringMatching(/\/api\/developmentbooks\/getBooks/)
     );
   });
 });
